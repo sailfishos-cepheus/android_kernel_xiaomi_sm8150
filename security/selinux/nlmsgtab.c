ae1ca919207a1c42593f7bd3c8e3aa0753404e6b
Mattia <mattia@gmail.com>
2026-07-05 15:08:15 +0200

    Fix compilation error with selinux

--- security/selinux/nlmsgtab.c
+++ security/selinux/nlmsgtab.c
@@ -149,7 +149,7 @@ static int nlmsg_perm(u16 nlmsg_type, u32 *perm, const struct nlmsg_perm *tab, s
 			break;
 		}
 
-	return err;
+		return err;
 }
 
 int selinux_nlmsg_lookup(u16 sclass, u16 nlmsg_type, u32 *perm)
@@ -157,51 +157,51 @@ int selinux_nlmsg_lookup(u16 sclass, u16 nlmsg_type, u32 *perm)
 	int err = 0;
 
 	switch (sclass) {
-	case SECCLASS_NETLINK_ROUTE_SOCKET:
-		/* RTM_MAX always point to RTM_SETxxxx, ie RTM_NEWxxx + 3 */
-		BUILD_BUG_ON(RTM_MAX != (RTM_NEWCACHEREPORT + 3));
-		err = nlmsg_perm(nlmsg_type, perm, nlmsg_route_perms,
-				 sizeof(nlmsg_route_perms));
-		break;
+		case SECCLASS_NETLINK_ROUTE_SOCKET:
+			/* RTM_MAX always point to RTM_SETxxxx, ie RTM_NEWxxx + 3 */
+			BUILD_BUG_ON(RTM_MAX != (RTM_NEWCACHEREPORT + 3));
+			err = nlmsg_perm(nlmsg_type, perm, nlmsg_route_perms,
+							 sizeof(nlmsg_route_perms));
+			break;
 
-	case SECCLASS_NETLINK_TCPDIAG_SOCKET:
-		err = nlmsg_perm(nlmsg_type, perm, nlmsg_tcpdiag_perms,
-				 sizeof(nlmsg_tcpdiag_perms));
-		break;
+		case SECCLASS_NETLINK_TCPDIAG_SOCKET:
+			err = nlmsg_perm(nlmsg_type, perm, nlmsg_tcpdiag_perms,
+							 sizeof(nlmsg_tcpdiag_perms));
+			break;
 
-	case SECCLASS_NETLINK_XFRM_SOCKET:
-		BUILD_BUG_ON(XFRM_MSG_MAX != XFRM_MSG_MAPPING);
-		err = nlmsg_perm(nlmsg_type, perm, nlmsg_xfrm_perms,
-				 sizeof(nlmsg_xfrm_perms));
-		break;
+		case SECCLASS_NETLINK_XFRM_SOCKET:
+			BUILD_BUG_ON(XFRM_MSG_MAX != XFRM_MSG_MAPPING);
+			err = nlmsg_perm(nlmsg_type, perm, nlmsg_xfrm_perms,
+							 sizeof(nlmsg_xfrm_perms));
+			break;
 
-	case SECCLASS_NETLINK_AUDIT_SOCKET:
-		if ((nlmsg_type >= AUDIT_FIRST_USER_MSG &&
-		     nlmsg_type <= AUDIT_LAST_USER_MSG) ||
-		    (nlmsg_type >= AUDIT_FIRST_USER_MSG2 &&
-		     nlmsg_type <= AUDIT_LAST_USER_MSG2)) {
-			*perm = NETLINK_AUDIT_SOCKET__NLMSG_RELAY;
-		} else {
-			err = nlmsg_perm(nlmsg_type, perm, nlmsg_audit_perms,
-					 sizeof(nlmsg_audit_perms));
-		}
-		break;
+		case SECCLASS_NETLINK_AUDIT_SOCKET:
+			if ((nlmsg_type >= AUDIT_FIRST_USER_MSG &&
+				nlmsg_type <= AUDIT_LAST_USER_MSG) ||
+				(nlmsg_type >= AUDIT_FIRST_USER_MSG2 &&
+				nlmsg_type <= AUDIT_LAST_USER_MSG2)) {
+				*perm = NETLINK_AUDIT_SOCKET__NLMSG_RELAY;
+				} else {
+					err = nlmsg_perm(nlmsg_type, perm, nlmsg_audit_perms,
+									 sizeof(nlmsg_audit_perms));
+				}
+				break;
 
-	/* No messaging from userspace, or class unknown/unhandled */
-	default:
-		err = -ENOENT;
-		break;
+			/* No messaging from userspace, or class unknown/unhandled */
+			default:
+				err = -ENOENT;
+				break;
 	}
 
 	return err;
 }
 
-static void nlmsg_set_perm_for_type(u32 perm, u16 type)
+static void nlmsg_set_getlink_perm(u32 perm)
 {
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(nlmsg_route_perms); i++) {
-		if (nlmsg_route_perms[i].nlmsg_type == type) {
+		if (nlmsg_route_perms[i].nlmsg_type == RTM_GETLINK) {
 			nlmsg_route_perms[i].perm = perm;
 			break;
 		}
@@ -211,27 +211,11 @@ static void nlmsg_set_perm_for_type(u32 perm, u16 type)
 /**
  * Use nlmsg_readpriv as the permission for RTM_GETLINK messages if the
  * netlink_route_getlink policy capability is set. Otherwise use nlmsg_read.
- * Similarly, use nlmsg_getneigh for RTM_GETNEIGH and RTM_GETNEIGHTBL if the
- * netlink_route_getneigh policy capability is set. Otherwise use nlmsg_read.
  */
 void selinux_nlmsg_init(void)
 {
 	if (selinux_android_nlroute_getlink())
-		nlmsg_set_perm_for_type(NETLINK_ROUTE_SOCKET__NLMSG_READPRIV,
-					RTM_GETLINK);
+		nlmsg_set_getlink_perm(NETLINK_ROUTE_SOCKET__NLMSG_READPRIV);
 	else
-		nlmsg_set_perm_for_type(NETLINK_ROUTE_SOCKET__NLMSG_READ,
-					RTM_GETLINK);
-
-	if (selinux_android_nlroute_getneigh()) {
-		nlmsg_set_perm_for_type(NETLINK_ROUTE_SOCKET__NLMSG_GETNEIGH,
-					RTM_GETNEIGH);
-		nlmsg_set_perm_for_type(NETLINK_ROUTE_SOCKET__NLMSG_GETNEIGH,
-					RTM_GETNEIGHTBL);
-	} else {
-		nlmsg_set_perm_for_type(NETLINK_ROUTE_SOCKET__NLMSG_READ,
-					RTM_GETNEIGH);
-		nlmsg_set_perm_for_type(NETLINK_ROUTE_SOCKET__NLMSG_READ,
-					RTM_GETNEIGHTBL);
-	}
+		nlmsg_set_getlink_perm(NETLINK_ROUTE_SOCKET__NLMSG_READ);
 }
